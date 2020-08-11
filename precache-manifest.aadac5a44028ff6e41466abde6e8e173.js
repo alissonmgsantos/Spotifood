11720aa8d80a14713d74b389b76c5afc098bc801
Alisson Matos <alissonmgsantos@gmail.com>
2020-08-11 20:36:24 -0300

    Updates

---
+++ precache-manifest.aadac5a44028ff6e41466abde6e8e173.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eebc7e5b70779055e3a584af3d19a975",
+    "url": "/index.html"
+  },
+  {
+    "revision": "861e4de6b8889c7db44f",
+    "url": "/static/css/main.e139f90e.chunk.css"
+  },
+  {
+    "revision": "e9c40da95dd8b52c773a",
+    "url": "/static/js/2.78bb8ec4.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/static/js/2.78bb8ec4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "861e4de6b8889c7db44f",
+    "url": "/static/js/main.63e5f557.chunk.js"
+  },
+  {
+    "revision": "3ad5752d8c5920c8d339",
+    "url": "/static/js/runtime-main.62a68f07.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/spotifood/precache-manifest.d2514a4397511c3e5a84a4dd4e03cc38.js"
+  "/precache-manifest.aadac5a44028ff6e41466abde6e8e173.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/spotifood/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });

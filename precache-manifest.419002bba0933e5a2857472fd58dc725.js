c0c2989510b6fbca5a7bbac82f5ec2aa78f438c5
Alisson Matos <alissonmgsantos@gmail.com>
2020-08-11 20:17:50 -0300

    Updates

---
+++ precache-manifest.419002bba0933e5a2857472fd58dc725.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d835814076a1e268ec3e2ced28da3c0",
+    "url": "/spotifood/index.html"
+  },
+  {
+    "revision": "cee5faa0e5c7188f3c02",
+    "url": "/spotifood/static/css/2.18fb2ca0.chunk.css"
+  },
+  {
+    "revision": "ca907e398e60016e8c5a",
+    "url": "/spotifood/static/css/main.e139f90e.chunk.css"
+  },
+  {
+    "revision": "cee5faa0e5c7188f3c02",
+    "url": "/spotifood/static/js/2.8d038c36.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/spotifood/static/js/2.8d038c36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca907e398e60016e8c5a",
+    "url": "/spotifood/static/js/main.6a8efac1.chunk.js"
+  },
+  {
+    "revision": "a079865597fea7798ce2",
+    "url": "/spotifood/static/js/runtime-main.a3b371ce.js"
+  }
+]);
